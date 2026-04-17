d7da2c4a92a42e1249d307a64faa04d3fde4739c
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-17 19:36:38 +0200

    refactor: simplify BookDetails component by removing unused code and fetching logic

--- BooksLibrary/bookslibrary.client/src/app/books/[id]/page.tsx
+++ BooksLibrary/bookslibrary.client/src/app/books/[id]/page.tsx
@@ -1,16 +1,14 @@
-import { notFound } from 'next/navigation'
+import { notFound } from 'next/navigation';
 import { BookDetailsPage } from '../BookDetailsPage';
 import BookDetailPageHead from './BookDetailPageHead';
 import { Metadata } from 'next';
-import { Book } from '../types/book';
+import { getBookById } from '@/services/books/booksApi';
 
 export const metadata: Metadata = {
     metadataBase: new URL('https://yoursite.com'),
-
     title: 'Book Library | Book Details',
     description:
         'This book will take you on a journey through the life of Kvothe, a gifted musician and magician, as he navigates a world filled with danger, magic, and mystery.',
-
     openGraph: {
         title: 'Book Library | Book Details',
         description:
@@ -30,31 +28,16 @@ export const metadata: Metadata = {
     },
 };
 
-//type Book = {
-//    bookId: string;
-//    title: string;
-//    authors: string;
-//    createdAtUtc: string;
-//};
-
 type Props = {
-    params: Promise<{
-        id: string;
-    }>;
+    params: Promise<{ id: string }>;
     className?: string;
 };
 
 export default async function BookDetails({ params, className }: Props) {
     const { id } = await params;
 
-    const res = await fetch(`https://localhost:7263/api/books/${id}`, {
-        cache: 'no-store',
-    });
-
-    if (!res.ok) return notFound();
-
-    const book: Book = await res.json();
-    //const book = await res.json();
+    const book = await getBookById(id).catch(() => null);
+    if (!book) return notFound();
 
     return (
         <section
@@ -63,7 +46,6 @@ export default async function BookDetails({ params, className }: Props) {
                 .join(' ')}
         >
             <BookDetailPageHead className="mb-6" />
-
             <div className="flex flex-col gap-4 text-light-bg">
                 <BookDetailsPage {...book} />
             </div>
