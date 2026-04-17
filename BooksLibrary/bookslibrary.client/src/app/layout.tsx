7828f5f5bff09af43455c6a334ffc83c4ae1d770
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-17 19:31:51 +0200

    refactor: remove unused SEO metadata and favicon settings from RootLayout

--- BooksLibrary/bookslibrary.client/src/app/layout.tsx
+++ BooksLibrary/bookslibrary.client/src/app/layout.tsx
@@ -5,42 +5,6 @@ import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import Sidebar from '@/components/layout/Sidebar';
 import { usePathname } from 'next/navigation';
-//import { Metadata } from 'next';
-
-// Global SEO metadata (applies to all pages unless overridden)
-// TODO: Client components (use client) such as RootLayout and AllBooksPage: They cannot have metadata. "use client or use server"
-//const metadata: Metadata = {
-//  metadataBase: new URL('https://yoursite.com'),
-
-//  title: {
-//    default: 'Book Library',
-//    template: '%s | Book Library',
-//  },
-
-//  description:
-//    'Book Library is a modern web application built with Next.js and Tailwind CSS, focused on performance, accessibility, and clean UI.',
-
-//  openGraph: {
-//    title: 'Book Library',
-//    description: 'Book Library is a modern web application built with Next.js and Tailwind CSS.',
-//    url: 'https://yoursite.com',
-//    siteName: 'Book Library',
-//    locale: 'en_US',
-//    type: 'website',
-//    images: [
-//      {
-//        url: '/og-image.png',
-//        width: 1200,
-//        height: 630,
-//        alt: 'Book Library',
-//      },
-//    ],
-//  },
-
-//  icons: {
-//    icon: '/favicon.ico',
-//  },
-//};
 
 // Main container component for the app with grid layout and font styles applied to the body
 // This layout will be used across all pages in the app, providing a consistent structure and styling
@@ -65,11 +29,6 @@ export default function RootLayout({ children }: Props) {
 
   return (
     <html lang="en">
-      {/* Method manually setting the favicon in Next.js */}
-      {/* <head>
-      <link rel="icon" href="/favicon.ico" type="image/svg+xml" />
-      <title>Book Library</title>
-      </head> */}
       <body>
         <div className="grid min-h-screen grid-rows-[auto_1fr_auto] grid-cols-[250px_1fr]">
           <Header className="col-span-2" />
