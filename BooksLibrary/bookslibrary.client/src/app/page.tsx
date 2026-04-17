c678292e2758215437758d62e0eeadd2e44369eb
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-17 19:35:37 +0200

    refactor: replace BookShelfTest with BookShelf component in Home page

--- BooksLibrary/bookslibrary.client/src/app/page.tsx
+++ BooksLibrary/bookslibrary.client/src/app/page.tsx
@@ -1,15 +1,12 @@
 import LibraryPageHead from '@/components/home/LibraryPageHead';
-import BookShelfTest from '@/components/home/BookShelfTest';
+import BookShelf from '@/components/home/BookShelf';
 
 // This is the main page of the app, rendered at the root URL ("/").
-// It imports and uses various components to display the library dashboard, including a header, a bookshelf, and other sections that can be added later.
-// The layout and styling are consistent with the rest of the app, as defined in the RootLayout component.
 export default function Home() {
   return (
     <div>
       <LibraryPageHead className="mb-6" />
-          <BookShelfTest />
-          {/*<BookShelf className="border mb-6" />*/}
+      <BookShelf className="border mb-6" />
     </div>
   );
 }
