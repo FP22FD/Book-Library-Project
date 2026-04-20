a742ba81ea5d551d7570a366dd640dcd23db7046
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-20 15:44:19 +0200

    feat: Extend BookResponse to include additional properties for comprehensive book details

--- BooksLibrary/BooksLibrary.Server/Requests/BookResponse.cs
+++ BooksLibrary/BooksLibrary.Server/Requests/BookResponse.cs
@@ -2,12 +2,23 @@
 
 namespace BooksLibrary.Server.Requests;
 
-public record BookResponse(Guid BookId, string Title, string Authors, DateTimeOffset CreatedAtUtc);
+public record BookResponse(Guid BookId, string Title, string Authors, DateTimeOffset CreatedAtUtc, string? LanguageCode, string? Category, int? PageCount, string? ISBN, string? Description, string? ThumbnailUrl);
 
 public static class BookResponseExtension
 {
     public static BookResponse ToResponse(this BookResult bookResult)
     {
-        return new BookResponse(bookResult.BookId, bookResult.Title, bookResult.Authors, bookResult.CreatedAtUtc);
-    } 
+        return new BookResponse(
+            bookResult.BookId,
+            bookResult.Title,
+            bookResult.Authors,
+            bookResult.CreatedAtUtc,
+            bookResult.LanguageCode,
+            bookResult.Category,
+            bookResult.PageCount,
+            bookResult.ISBN,
+            bookResult.Description,
+            bookResult.ThumbnailUrl
+            );
+    }
 }
