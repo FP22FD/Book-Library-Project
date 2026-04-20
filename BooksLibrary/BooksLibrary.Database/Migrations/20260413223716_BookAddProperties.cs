648438cea5dcea011dcacad12faada4ac5d5dd53
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-20 15:38:14 +0200

    feat: add new properties to Book model for category, ISBN, language code, and page count

---
+++ BooksLibrary/BooksLibrary.Database/Migrations/20260413223716_BookAddProperties.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksLibrary.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookAddProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageCode",
+                table: "Books",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PageCount",
+                table: "Books",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageCode",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PageCount",
+                table: "Books");
+        }
+    }
+}
