a7be3349cfa3c25e56ab26f175859933f8b1b40d
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-23 14:41:38 +0200

    fix: Reorder error and book not found messages in BookDetailsClient for better user experience

--- BooksLibrary/bookslibrary.client/src/app/books/[id]/BookDetailsClient.tsx
+++ BooksLibrary/bookslibrary.client/src/app/books/[id]/BookDetailsClient.tsx
@@ -10,9 +10,9 @@ type Props = {
 export default function BookDetailsClient({ id }: Props) {
     const { book, error, loading } = useBookById(id);
 
-    if (!book) { return <p className="text-red-500">Book not found.</p> }
-    if (error) return <p className="text-red-500">{error}</p>;
     if (loading) return <p>Loading...</p>;
+    if (error) return <p className="text-red-500">{error}</p>;
+    if (!book) { return <p className="text-red-500">Book not found.</p> }
 
     return (
         <div className="flex flex-col gap-4 text-light-bg">
