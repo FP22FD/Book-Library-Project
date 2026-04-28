83fb54dda360c1b07cf61a80c6855899f1f96012
FP22FD <112474910+FP22FD@users.noreply.github.com>
2026-04-28 21:58:41 +0200

    feat: add AverageRating column to Books table and update model snapshot

---
+++ BooksLibrary/BooksLibrary.Database/Migrations/20260428103600_AddAverageRating.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksLibrary.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAverageRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AverageRating",
+                table: "Books",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "Books");
+        }
+    }
+}
